d2346a89e582687e3da51f088d6554aa08e3ff5b
tygowillems2002 <tygowillemsps5@outlook.com>
2025-02-03 12:27:04 +0100

    db.php update
    heb de goeie database geconnect

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 class DB {
     public $pdo;
 
-    public function __construct($db = "test", $user = "root", $pwd = "", $host = "localhost") {
+    public function __construct($db = "pdop5", $user = "root", $pwd = "", $host = "localhost") {
         try {
             $this->pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
